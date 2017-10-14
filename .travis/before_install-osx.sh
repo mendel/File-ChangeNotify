4689249e8a389580a45b3fe702ec5850f0920e29
Norbert Buchmuller <norbi@nix.hu>
2017-10-14 20:05:48 +0200

    make chown of ~/perl5 conditional

--- .travis/before_install-osx.sh
+++ .travis/before_install-osx.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 set -e
+set +H	# disable !foo style history expansion - no need to quote '!'
 
 rvm system
 brew update
 brew install perl
 perl -V
 
-sudo chown -R $USER /Users/travis/perl5
+if [ ! -O /Users/travis/perl5 ]; then
+	sudo chown -R $USER /Users/travis/perl5
+fi
+
 curl -L https://install.perlbrew.pl | bash
 source ~/perl5/perlbrew/etc/bashrc
 echo 'test -f ~/perl5/perlbrew/etc/bashrc && source ~/perl5/perlbrew/etc/bashrc' >>~/.bashrc
