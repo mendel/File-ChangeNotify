adc69697560aa3cb9465cae62489bdcef64e2895
Norbert Buchmuller <norbi@nix.hu>
2017-10-17 07:25:16 +0200

    fix PERLBREW_ROOT vs PERL5_ROOT confusion

--- .travis/before_install-osx.sh
+++ .travis/before_install-osx.sh
@@ -9,17 +9,17 @@ brew update
 brew install perl
 perl -V
 
-PERLBREW_ROOT=~/perl5
-if [ -d $PERLBREW_ROOT -a ! -O $PERLBREW_ROOT ]; then
+perl5_root=~/perl5
+if [ -d $perl5_root -a ! -O $perl5_root ]; then
 	# apparently the pre-installed (by Travis infra) ~/perl5 and its
 	# subdirs are owned by root ATM
-	PERLBREW_ROOT=~/perl5-local
+	perl5_root=~/perl5-local
 fi
-export PERLBREW_ROOT
+export PERLBREW_ROOT=$perl5_root/perlbrew
 
 curl -L https://install.perlbrew.pl | bash
-source $PERLBREW_ROOT/perlbrew/etc/bashrc
-echo 'test -f $PERLBREW_ROOT/perlbrew/etc/bashrc && source $PERLBREW_ROOT/perlbrew/etc/bashrc' >>~/.bashrc
+source $PERLBREW_ROOT/etc/bashrc
+echo 'test -f $PERLBREW_ROOT/etc/bashrc && source $PERLBREW_ROOT/etc/bashrc' >>~/.bashrc
 
 perlbrew list
 
@@ -28,7 +28,7 @@ perlbrew switch $TRAVIS_PERL_VERSION
 # some versions of the pre-installed (by Travis infra) perlbrew perls are
 # broken (Config.pm missing)
 if ! perl -V; then
-	rm -rf "$PERLBREW_ROOT"
+	rm -rf "$perl5_root"
 	perlbrew init
 	perlbrew install $TRAVIS_PERL_VERSION
 	perlbrew switch $TRAVIS_PERL_VERSION
