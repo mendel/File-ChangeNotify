f6010f00f5476728cfe3d22cb4a0b00bea0c7e9d
Norbert Buchmuller <norbi@nix.hu>
2017-10-18 10:27:58 +0200

    show statements executed

--- .travis/before_install-osx.sh
+++ .travis/before_install-osx.sh
@@ -3,36 +3,44 @@
 set -e
 set +H	# disable !foo style history expansion - no need to quote '!'
 
+_run()
+{
+	echo "$ $*" >&2
+	"$@"
+}
+
 install_perlbrew_and_perl()
 {
-	curl -L https://install.perlbrew.pl | bash
-	source $PERLBREW_ROOT/etc/bashrc
+	_run curl -L https://install.perlbrew.pl | bash
+	_run source $PERLBREW_ROOT/etc/bashrc
 	echo 'test -f $PERLBREW_ROOT/etc/bashrc && source $PERLBREW_ROOT/etc/bashrc' >>~/.bashrc
 
-	perlbrew list
+	_run perlbrew list
 
-	if ! perlbrew switch $TRAVIS_PERL_VERSION; then
-		perl_version=$(perlbrew available | sed -nE 's/^\s*((perl-)?'$TRAVIS_PERL_VERSION'\.\S+).*$/\1/p' | head -1)
+	if ! _run perlbrew switch $TRAVIS_PERL_VERSION; then
+		_run perlbrew available
 
-		perlbrew install $perl_version
+		perl_version=$(perlbrew available | sed -nE 's/^\s*((perl-)?'$TRAVIS_PERL_VERSION'\.\d+).*$/\1/p' | head -1)
 
-		perlbrew alias create $perl_version $TRAVIS_PERL_VERSION
+		_run perlbrew install $perl_version
 
-		perlbrew switch $TRAVIS_PERL_VERSION
+		_run perlbrew alias create $perl_version $TRAVIS_PERL_VERSION
+
+		_run perlbrew switch $TRAVIS_PERL_VERSION
 	fi
 }
 
-rvm system
-brew update
-brew install perl
-perl -V
+_run rvm system
+_run brew update
+_run brew install perl
+_run perl -V
 
 perl5_root=~/perl5
 if [ -d $perl5_root -a ! -O $perl5_root ]; then
 	# the pre-installed (by Travis infra) ~/perl5 and its subdirs are owned
 	# by root due to a Travis bug
 
-	cp -a ~/perl5 ~/perl5-local
+	_run cp -a ~/perl5 ~/perl5-local
 
 	perl5_root=~/perl5-local
 fi
@@ -43,7 +51,7 @@ install_perlbrew_and_perl
 # some versions of the pre-installed (by Travis infra) perlbrew perls are
 # broken (Config.pm missing)
 if ! perl -V; then
-	rm -rf "$perl5_root"
+	_run rm -rf "$perl5_root"
 
 	install_perlbrew_and_perl
 fi
